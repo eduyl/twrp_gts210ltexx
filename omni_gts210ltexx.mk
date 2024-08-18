da6bd640e4f65711b93f221b24db09ade52e08fd
eduyl <proman312008@gmail.com>
2024-08-18 14:35:28 +0530

    Initial commit

---
+++ omni_gts210ltexx.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from gts210ltexx device
+$(call inherit-product, device/samsung/gts210ltexx/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gts210ltexx
+PRODUCT_NAME := omni_gts210ltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T815
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := samsung SM-T815
